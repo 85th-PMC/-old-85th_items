b917b9de8424091e02b1f4ffa8f4ff5b40c36537
Snak3Doc <benjaminbadham@gmail.com>
2018-09-11 23:05:05 +1000

    Initial Commit

---
+++ config.cpp
@@ -0,0 +1,18 @@
+class cfgWeapons
+{
+	class ItemCore;
+    class FirstAidKit: ItemCore
+	class 85th_FAK: FirstAidKit
+    {
+        displayName="(85th) FAK"
+    };
+};
+
+{
+    class ItemCore;
+    class Medikit: ItemCore
+    class 85th_Medkit: Medikit
+    {
+        displayName="85th Medkit"
+    };
+};
